600a325356fa9d7234d73dd45b13d0b9f3540767
beze1 <beze1@protonmail.com>
2022-04-30 19:39:20 -0400

    create a 16x16 grid of square divs

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 (function () {
 
+    let main = document.querySelector('main');
+
+    for (let i = 0; i < 16 * 16; i++) {
+        let div = document.createElement('div');
+        main.appendChild(div);
+    }
+
+
 })()
