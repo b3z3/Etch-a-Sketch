35da5ceb30794ad2addf95d4cdec445798ff8955
beze1 <beze1@protonmail.com>
2022-05-01 00:34:50 -0400

    generateCanvas

--- js/app.js
+++ js/app.js
@@ -1,11 +1,46 @@
 (function () {
 
-    let main = document.querySelector('main');
+    const generateCanvas = function (size = 16) {
+        const main = document.querySelector('.canvas-container');
 
-    for (let i = 0; i < 16 * 16; i++) {
-        let div = document.createElement('div');
-        main.appendChild(div);
-    }
+        main.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        main.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+        for (let i = 0; i < size * size; i++) {
+            let div = document.createElement('div');
+            div.classList.add('cell');
+            main.appendChild(div);
+        }
+
+        let cells = document.querySelectorAll('.cell');
+
+        cells.forEach(cell => {
+            cell.addEventListener('mouseover', (e) => {
+                e.target.style.backgroundColor = 'orange';
+            });
+
+            // cell.addEventListener('mouseout', (e) => {
+            //     e.target.style.backgroundColor = 'white';
+            // });
+        });
+    };
+
+    generateCanvas();
+
+    // const getCells = function () {
+    //     return document.querySelectorAll('.cell');
+    // };
+
+    const changeSize = document.querySelector('#change-size');
+
+    changeSize.addEventListener('click', () => {
+        const main = document.querySelector('.canvas-container');
+        main.innerHTML = '';
+
+        let newSize = +prompt('Enter new size:', 16);
+        if (newSize > 0 && newSize < 100) {
+            generateCanvas(newSize);
+        }
+    });
 
 })()
